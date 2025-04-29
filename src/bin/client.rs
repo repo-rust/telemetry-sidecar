f085822f24271f4e633aaf6affa33a047a199522
Maksym Stepanenko <stepanenkomaksi@gmail.com>
2025-04-29 10:15:30 +0300

    Gracefull shutdown added using SIGTERM listener

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,9 +1,11 @@
-use anyhow::Context;
+use anyhow::{bail, Context};
 use chrono::Utc;
 use std::time::Duration;
 use telemetry_sidecar::unix_domain_socket_path;
 use tokio::io::AsyncWriteExt;
 use tokio::net::UnixStream;
+use tokio::select;
+use tokio::signal::unix::{signal, SignalKind};
 use tokio::time::sleep;
 
 #[tokio::main]
@@ -21,6 +23,9 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
     https://github.com/prometheus/docs/blob/main/content/docs/instrumenting/exposition_formats.md
     */
 
+    let mut sigterm =
+        signal(SignalKind::terminate()).context("failed to register SIGTERM handler")?;
+
     for i in 0..1_000_000 {
         // Unix timestamp in ms
         let timestamp_ms = Utc::now().timestamp_millis();
@@ -35,20 +40,21 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
                 "{{method=\"post\",code=\"200\",region=\"us-ashburn-1\"}} 123 {}\n",
                 timestamp_ms
             );
+        }
 
-            stream
-                .write_all(metric.as_bytes())
-                .await
-                .context("Failed to send message")?;
-
-            println!("Bad metric {} sent", i + 1,);
-        } else {
-            stream
-                .write_all(metric.as_bytes())
-                .await
-                .context("Failed to send message")?;
-
-            println!("Metric {} sent", i + 1,);
+        select! {
+            _ = sigterm.recv() => {
+                println!("SIGTERM received");
+                break;
+            }
+            write_result = stream.write_all(metric.as_bytes()) => {
+                if write_result.is_err() {
+                    bail!("Failed to send message")
+                }
+                else {
+                    println!("Metric {} sent", i + 1,);
+                }
+            }
         }
 
         sleep(Duration::from_secs(1)).await;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
-mod lib;
 mod line_protocol;
 
-use crate::lib::unix_domain_socket_path;
 use crate::line_protocol::Measurement;
 use anyhow::Context;
 use std::path::Path;
 use std::time::Duration;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::UnixListener;
-use tokio::time::sleep;
+use tokio::signal::unix::{signal, SignalKind};
+use tokio_util::sync::CancellationToken;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<(), anyhow::Error> {
-    let metrics_socket_path = &unix_domain_socket_path();
+    let metrics_socket_path = &telemetry_sidecar::unix_domain_socket_path();
 
     if Path::new(metrics_socket_path).exists() {
         tokio::fs::remove_file(metrics_socket_path)
@@ -23,7 +22,7 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
             ))?;
     }
 
-    let listener = UnixListener::bind(&metrics_socket_path).context(format!(
+    let listener = UnixListener::bind(metrics_socket_path).context(format!(
         "Can't listen on socket file '{}'",
         metrics_socket_path
     ))?;
@@ -33,42 +32,85 @@ async fn main() -> anyhow::Result<(), anyhow::Error> {
         metrics_socket_path
     );
 
-    // Spawn metrics publisher thread
+    let cancellation_token = CancellationToken::new();
+
+    let cancellation_token_copy = cancellation_token.clone();
     tokio::spawn(async move {
-        println!("Metrics publisher started");
-        loop {
-            sleep(Duration::from_secs(10)).await;
-            println!("Metrics publisher checking for new metrics to publish");
+        println!("Graceful shutdown listener started");
+
+        match signal(SignalKind::terminate()) {
+            Ok(mut sigterm) => {
+                sigterm.recv().await;
+                println!("SIGTERM received!");
+                cancellation_token_copy.cancel();
+            }
+            Err(err) => {
+                eprintln!("Unable to listen for SIGTERM signal: {}", err);
+                // we also shut down in case of error
+                cancellation_token_copy.cancel();
+            }
         }
     });
 
-    loop {
-        //
-        // Accept Unix socket connection (we expect to have only one per sidecar) and handle all
-        // write operations in the main thread
-        //
-        let (stream, _) = listener.accept().await.context("Can't accept connection")?;
-
-        println!("New client connected");
-
-        let reader = BufReader::new(stream);
-        let mut lines = reader.lines();
-
-        while let Ok(Some(mut line)) = lines.next_line().await {
-            if line.ends_with('\n') {
-                line.pop();
-            }
-
-            match Measurement::new(&line) {
-                Ok(measurement) => {
-                    println!("Received measurement: {:?}", measurement);
+    // Spawn metrics publisher thread
+    let cancellation_token_copy = cancellation_token.clone();
+    tokio::spawn(async move {
+        println!("Metrics publisher started");
+        loop {
+            tokio::select! {
+                _ = cancellation_token_copy.cancelled() => {
+                    println!("Metrics publisher interrupted");
+                    break;
                 }
-                Err(error_msg) => {
-                    println!("Error during measurement processing: {}", error_msg);
+                _ = tokio::time::sleep(Duration::from_secs(10)) => {
+                     println!("Metrics publisher checking for new metrics to publish");
                 }
             }
         }
+    });
 
-        println!("Client connection closed.");
+    //
+    // Accept Unix socket connection (we expect to have only one per sidecar) and handle all
+    // write operations in the main thread
+    //
+    let (stream, _) = listener.accept().await.context("Can't accept connection")?;
+
+    println!("New client connected");
+
+    let reader = BufReader::new(stream);
+    let mut lines = reader.lines();
+
+    let cancellation_token_copy = cancellation_token.clone();
+    loop {
+        tokio::select! {
+            _ = cancellation_token_copy.cancelled() => {
+                println!("Main read loop interrupted");
+                break;
+            }
+            result = lines.next_line() => {
+                if let Ok(Some(mut line)) = result {
+                     if line.ends_with('\n') {
+                        line.pop();
+                    }
+
+                    match Measurement::new(&line) {
+                        Ok(measurement) => {
+                            println!("Received measurement: {:?}", measurement);
+                        }
+                        Err(error_msg) => {
+                            println!("Error during measurement processing: {}", error_msg);
+                        }
+                    }
+                }
+                else {
+                    println!("Read loop error");
+                    break;
+                }
+            }
+        }
     }
+
+    println!("Client connection closed.");
+
+    Ok(())
 }
